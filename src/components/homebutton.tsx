2f65b688770e71e95dd0f0e26b630b072c552528
suk-6 <me@suk.kr>
2024-05-14 20:58:40 +0900

    Refactor HomeButton component for improved readability and styling

--- src/components/homebutton.tsx
+++ src/components/homebutton.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {
 
 export const HomeButton = ({ text, color, func }: ButtonProps) => {
 	let buttonClass =
-		"w-full h-full bg-green-400 text-center text-black text-[22px] font-semibold font-['Pretendard'] flex items-center justify-center";
+		"w-full h-full bg-green-400 text-center text-black text-xl font-semibold font-['Pretendard'] flex items-center justify-center";
 	if (color === "green") {
 		buttonClass += " bg-green-400";
 	} else if (color === "red") {
@@ -14,7 +14,7 @@ export const HomeButton = ({ text, color, func }: ButtonProps) => {
 	}
 	return (
 		<div
-			className=" w-[195px] h-[93px] mb-1 relative flex justify-center items-center rounded-[20px] overflow-hidden"
+			className=" w-[90%] h-[50%] mb-1 relative flex justify-center items-center rounded-[20px] overflow-hidden"
 			onClick={() => func()}
 		>
 			<div className={buttonClass}>{text}</div>
