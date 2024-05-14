9c414c002e6025af50d54f5db49fb7d043efc444
suk-6 <me@suk.kr>
2024-05-14 11:36:52 +0900

    feat: Add lazy loading for images and integrate @vercel/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { Analytics } from "@vercel/analytics/react";
+import Home from "./pages/home";
+import Login from "./pages/login";
 
 function App() {
 	return (
 		<>
-			<div className="flex flex-col align-center items-center">
-				<p className="">메이크 출입관리 시스템 개발 중</p>
-			</div>
+			<Home />
+			<Login />
 			<Analytics />
 		</>
 	);
