117f91cdf8bd98f33dd3e6f85c989027c61a3287
suk-6 <me@suk.kr>
2024-05-14 10:20:08 +0900

    Chore: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Analytics } from "@vercel/analytics/react";
 function App() {
 	return (
 		<>
-			<p>메이크 출입관리 시스템 개발 중</p>
+			<div className="flex flex-col align-center items-center">
+				<p className="">메이크 출입관리 시스템 개발 중</p>
+			</div>
 			<Analytics />
 		</>
 	);
