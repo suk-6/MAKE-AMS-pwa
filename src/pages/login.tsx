a2138c870917ae8c6c9ab76f0caf338f1e37aeca
suk-6 <me@suk.kr>
2024-05-14 11:37:06 +0900

    feat: Add login page component

---
+++ src/pages/login.tsx
@@ -0,0 +1,24 @@
+import { Textbox } from "../components/textbox";
+
+const Login = () => {
+	return (
+		<div className="Page2 w-[430px] h-[932px] relative bg-white">
+			<div className=" w-[254px] h-[31px] left-[13px] top-[40px] absolute text-black text-[26px] font-bold font-['Pretendard']">
+				로그인
+			</div>
+			<div className="Line1 w-[406px] h-[0px] left-[10px] top-[82px] absolute border border-black"></div>
+			<div className=" w-[230px] h-[50px] left-[100px] top-[420px] absolute">
+				<div className="Rectangle6 w-[230px] h-[50px] left-0 top-0 absolute bg-gray-200 rounded-[20px] border border-black" />
+				<div className=" left-[86px] top-[12px] absolute text-center text-black text-[22px] font-semibold font-['Pretendard']">
+					로그인
+				</div>
+			</div>
+			<div className="">
+				<Textbox inputBoxHint="아이디" inputBoxType="text" />
+				<Textbox inputBoxHint="비밀번호" inputBoxType="password" />
+			</div>
+		</div>
+	);
+};
+
+export default Login;
