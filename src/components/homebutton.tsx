5c6a1ff41f7d6b74f53f12b55693feb60dbbaf47
suk-6 <me@suk.kr>
2024-05-14 19:54:17 +0900

    Refactor HomeButton component to improve readability and remove unnecessary code

--- src/components/homebutton.tsx
+++ src/components/homebutton.tsx
@@ -4,16 +4,15 @@ export interface ButtonProps {
 }
 
 export const HomeButton = ({ text, color }: ButtonProps) => {
-	let buttonClass = "";
+	let buttonClass =
+		"w-full h-full bg-green-400 text-center text-black text-[22px] font-semibold font-['Pretendard'] flex items-center justify-center";
 	if (color === "green") {
-		buttonClass =
-			"Rectangle1 w-[195px] h-[93px] bg-green-400 rounded-[20px] text-center text-black text-[22px] font-semibold font-['Pretendard'] flex items-center justify-center";
+		buttonClass += " bg-green-400";
 	} else if (color === "red") {
-		buttonClass =
-			"Rectangle1 w-[195px] h-[93px] bg-red-400 rounded-[20px] text-center text-black text-[22px] font-semibold font-['Pretendard'] flex items-center justify-center";
+		buttonClass += " bg-red-400";
 	}
 	return (
-		<div className=" w-[195px] h-[93px] mb-1 relative flex justify-center items-center">
+		<div className=" w-[195px] h-[93px] mb-1 relative flex justify-center items-center rounded-[20px] overflow-hidden">
 			<div className={buttonClass}>{text}</div>
 		</div>
 	);
