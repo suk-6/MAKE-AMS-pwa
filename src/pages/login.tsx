5b35018b583be4a4bee3f18325d298c06779db76
suk-6 <me@suk.kr>
2024-05-14 13:27:27 +0900

    chore: Update viewport meta tag for better mobile experience

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,14 +2,14 @@ import { Textbox } from "../components/textbox";
 
 const Login = () => {
 	return (
-		<div className="Page2 w-[430px] h-[932px] relative bg-white">
+		<div className="Page2 w-auto h-[932px] relative bg-white">
 			<div className=" w-[254px] h-[31px] left-[13px] top-[40px] absolute text-black text-[26px] font-bold font-['Pretendard']">
 				로그인
 			</div>
 			<div className="Line1 w-[406px] h-[0px] left-[10px] top-[82px] absolute border border-black"></div>
 			<div className=" w-[230px] h-[50px] left-[100px] top-[420px] absolute">
 				<div className="Rectangle6 w-[230px] h-[50px] left-0 top-0 absolute bg-gray-200 rounded-[20px] border border-black" />
-				<div className=" left-[86px] top-[12px] absolute text-center text-black text-[22px] font-semibold font-['Pretendard']">
+				<div className=" left-[85px] top-[11px] absolute text-center text-black text-[22px] font-semibold font-['Pretendard']">
 					로그인
 				</div>
 			</div>
