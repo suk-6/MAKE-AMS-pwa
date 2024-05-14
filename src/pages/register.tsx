0ff5394ecca81dd5f4d93f87ea18802b0bbc8dc4
suk-6 <me@suk.kr>
2024-05-15 01:03:49 +0900

    Refactor Register page layout for improved readability and styling

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Textbox } from "../components/textBox";
 import { register } from "../services/auth";
+import { TitleBar } from "../components/titleBar";
+import { AuthButton } from "../components/authButton";
 
 const Register = () => {
 	if (localStorage.getItem("code")) document.location.href = "/";
@@ -11,44 +13,40 @@ const Register = () => {
 	const [name, setName] = useState("");
 
 	return (
-		<div className="Page2 w-auto h-[932px] relative bg-white">
-			<div className=" w-[254px] h-[31px] left-[13px] top-[40px] absolute text-black text-[26px] font-bold font-['Pretendard']">
-				회원가입
-			</div>
-			<div className="Line1 w-[406px] h-[0px] left-[10px] top-[82px] absolute border border-black"></div>
-			<div className="">
-				<Textbox
-					inputBoxHint="아이디"
-					inputBoxType="text"
-					value={id}
-					changeValue={setId}
-				/>
-				<Textbox
-					inputBoxHint="비밀번호"
-					inputBoxType="password"
-					value={password}
-					changeValue={setPassword}
-				/>
-				<Textbox
-					inputBoxHint="학번"
-					inputBoxType="text"
-					value={studentId}
-					changeValue={setStudentId}
-				/>
-				<Textbox
-					inputBoxHint="이름"
-					inputBoxType="text"
-					value={name}
-					changeValue={setName}
-				/>
-			</div>
-			<div
-				className=" w-[230px] h-[50px] left-[100px] top-[580px] absolute flex justify-center items-center"
-				onClick={() => register(id, password, studentId, name)}
-			>
-				<div className="Rectangle6 w-[230px] h-[50px] left-0 top-0 absolute bg-gray-200 rounded-[20px] border border-black" />
-				<div className=" relative text-center text-black text-[22px] font-semibold font-['Pretendard']">
-					회원가입
+		<div className="fixed w-full h-full bg-white">
+			<TitleBar title="회원가입" />
+			<div className="grid grid-rows-3 h-4/6 gap-10">
+				<div className="grid grid-rows-4 gap-10 h-full row-span-2">
+					<Textbox
+						inputBoxHint="아이P디"
+						inputBoxType="text"
+						value={id}
+						changeValue={setId}
+					/>
+					<Textbox
+						inputBoxHint="비밀번호"
+						inputBoxType="password"
+						value={password}
+						changeValue={setPassword}
+					/>
+					<Textbox
+						inputBoxHint="학번"
+						inputBoxType="text"
+						value={studentId}
+						changeValue={setStudentId}
+					/>
+					<Textbox
+						inputBoxHint="이름"
+						inputBoxType="text"
+						value={name}
+						changeValue={setName}
+					/>
+				</div>
+				<div
+					className="flex items-center justify-center"
+					onClick={() => register(id, password, studentId, name)}
+				>
+					<AuthButton text="회원가입" />
 				</div>
 			</div>
 		</div>
