89b05524fb68bb2fab2196ba85a13d1ceee7a331
suk-6 <me@suk.kr>
2024-05-15 15:57:13 +0900

    Refactor adminFeature component for improved consistency and clarity

--- src/components/admin/adminFeature.tsx
+++ src/components/admin/adminFeature.tsx
@@ -5,9 +5,15 @@ export const AdminFeature = () => {
 	return (
 		<div className="h-[25%]">
 			<TitleBar title="관리자" />
-			<div className="grid grid-cols-2 h-[50%]">
-				<HomeButton text="잠금" color="green" func={() => {}} />
-				<HomeButton text="잠금" color="green" func={() => {}} />
+			<div className="grid grid-rows-2 grid-cols-2 h-5/6 gap-y-3">
+				<HomeButton text="강제 잠금" color="bg-red1" func={() => {}} />
+				<HomeButton text="해제" color="bg-green1" func={() => {}} />
+				<HomeButton text="가입 승인" color="bg-cyan1" func={() => {}} />
+				<HomeButton
+					text="출입 로그"
+					color="bg-yellow1"
+					func={() => {}}
+				/>
 			</div>
 		</div>
 	);
