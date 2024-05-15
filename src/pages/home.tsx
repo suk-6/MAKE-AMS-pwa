3b2b6a162db28a368f07ea4d5c07fa007167c8ca
suk-6 <me@suk.kr>
2024-05-15 22:16:22 +0900

    Feat: Home page to check admin status before rendering admin feature

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,24 @@
 import { HomeFeature } from "../components/homeFeature";
 import { TitleBar } from "../components/titleBar";
 import { AdminFeature } from "../components/admin/adminFeature";
+import { checkAdmin } from "../services/auth";
+import { useEffect, useState } from "react";
 
 const Home = () => {
 	if (!localStorage.getItem("code")) document.location.href = "/login";
+	const [isAdmin, setIsAdmin] = useState(false);
+
+	useEffect(() => {
+		checkAdmin().then((result) => {
+			setIsAdmin(result);
+		});
+	});
 
 	return (
 		<div className="fixed w-full h-full bg-white">
 			<TitleBar title="메이커스페이스 출입관리" />
 			<HomeFeature />
-			<AdminFeature />
+			{isAdmin && <AdminFeature />}
 		</div>
 	);
 };
