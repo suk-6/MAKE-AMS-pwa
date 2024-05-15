786dc09ba7e37e11632f6ec72faeca55a17da4fe
suk-6 <me@suk.kr>
2024-05-15 15:58:56 +0900

    Chore: adminFeature component for improved consistency and clarity

--- src/components/admin/adminFeature.tsx
+++ src/components/admin/adminFeature.tsx
@@ -6,13 +6,33 @@ export const AdminFeature = () => {
 		<div className="h-[25%]">
 			<TitleBar title="관리자" />
 			<div className="grid grid-rows-2 grid-cols-2 h-5/6 gap-y-3">
-				<HomeButton text="강제 잠금" color="bg-red1" func={() => {}} />
-				<HomeButton text="해제" color="bg-green1" func={() => {}} />
-				<HomeButton text="가입 승인" color="bg-cyan1" func={() => {}} />
+				<HomeButton
+					text="강제 잠금"
+					color="bg-red1"
+					func={() => {
+						alert("강제 잠금");
+					}}
+				/>
+				<HomeButton
+					text="해제"
+					color="bg-green1"
+					func={() => {
+						alert("해제");
+					}}
+				/>
+				<HomeButton
+					text="가입 승인"
+					color="bg-cyan1"
+					func={() => {
+						alert("가입 승인");
+					}}
+				/>
 				<HomeButton
 					text="출입 로그"
 					color="bg-yellow1"
-					func={() => {}}
+					func={() => {
+						alert("출입 로그");
+					}}
 				/>
 			</div>
 		</div>
