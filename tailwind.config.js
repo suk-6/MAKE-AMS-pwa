ab3176ab57675960067efa81bb36a229bba0ebff
suk-6 <me@suk.kr>
2024-05-15 15:57:06 +0900

    chore: Update tailwind.config.js with new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      black: '#000000',
+      red1: '#E65C5C',
+      blue1: '#5C93E6',
+      green1: '#5CE66A',
+      cyan1: '#81EEDB',
+      yellow1: '#EBF000',
+    },
     extend: {},
   },
   plugins: [],
