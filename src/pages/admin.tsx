1b019d6842103f02c11fa87b23f5c55e48008bc7
suk-6 <me@suk.kr>
2024-05-16 15:11:41 +0900

    feat: admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,9 @@
+import { AdminFeature } from "../components/admin/adminFeature";
+
+export const AdminView = () => {
+	return (
+		<div className="w-full h-full">
+			<AdminFeature />
+		</div>
+	);
+};
