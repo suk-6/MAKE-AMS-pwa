5c2243445dcf4115168233b958988edba0b1d574
suk-6 <me@suk.kr>
2024-05-16 14:32:42 +0900

    chore: Add blue2 color to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
       gray: colors.gray,
       red1: '#E65C5C',
       blue1: '#5C93E6',
+      blue2: '#6CB2FF',
       green1: '#5CE66A',
       cyan1: '#81EEDB',
       yellow1: '#EBF000',
