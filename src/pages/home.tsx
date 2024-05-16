00cad61b82bb27b9f651a6189fa635eddb120b0b
suk-6 <me@suk.kr>
2024-05-16 15:11:45 +0900

    feat: Add admin button to Home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,17 @@
 import { HomeFeature } from "../components/homeFeature";
 import { TitleBar } from "../components/titleBar";
-import { AdminFeature } from "../components/admin/adminFeature";
 import { checkAdmin } from "../services/auth";
 import { useEffect, useState } from "react";
 import { LockStatusBox } from "../components/lockStatusBox";
 import { getLockStatus } from "../services/admin";
+import { HomeButton } from "../components/homeButton";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
 	if (!localStorage.getItem("code")) document.location.href = "/login";
 	const [isAdmin, setIsAdmin] = useState(false);
 	const [lockStatus, setLockStatus] = useState(undefined);
+	const navigator = useNavigate();
 
 	useEffect(() => {
 		checkAdmin().then((result) => {
@@ -25,9 +27,18 @@ const Home = () => {
 		<div className="fixed w-full h-full bg-white">
 			<TitleBar title="메이커스페이스 출입관리" />
 			<LockStatusBox status={lockStatus} />
-			<div className="h-8"></div>
+			<div className="h-[3%]" />
 			<HomeFeature />
-			{isAdmin && <AdminFeature />}
+			<div className="h-[5%]" />
+			<div className="h-[10%]">
+				{isAdmin && (
+					<HomeButton
+						text="관리자 메뉴"
+						color="bg-gray-300"
+						func={() => navigator("/admin")}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
