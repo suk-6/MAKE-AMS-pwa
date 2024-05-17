e5478cfdf8fc00e3d6fb3b4935bb4c9f4135bd4d
suk-6 <me@suk.kr>
2024-05-17 10:44:31 +0900

    chore: Update App component to use InstallPage for app installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
@@ -8,60 +7,15 @@ import { Route, Routes } from "react-router-dom";
 import { AccessLogPage } from "./pages/accessLog";
 import { AccessRequestPage } from "./pages/accessRequest";
 import { QRView } from "./pages/qr";
-import { useEffect, useState } from "react";
 import { AdminView } from "./pages/admin";
+import { InstallPage } from "./pages/install";
 
-interface BeforeInstallPromptEvent extends Event {
-	readonly platforms: Array<string>;
-	readonly userChoice: Promise<{
-		outcome: "accepted" | "dismissed";
-		platform: string;
-	}>;
-	prompt(): Promise<void>;
-}
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
-	const [deferredPrompt, setDeferredPrompt] =
-		useState<BeforeInstallPromptEvent>();
-	const userAgent = navigator.userAgent.toLowerCase();
-
-	useEffect(() => {
-		window.addEventListener("beforeinstallprompt", (event) => {
-			event.preventDefault();
-			setDeferredPrompt(event as BeforeInstallPromptEvent);
-		});
-
-		if (userAgent.includes("kakaotalk")) {
-			location.href =
-				"kakaotalk://web/openExternal?url=" +
-				encodeURIComponent(window.location.toString());
-		}
-	}, [userAgent]);
-
-	const installApp = () => {
-		if (!deferredPrompt) {
-			if (userAgent.includes("iphone") || userAgent.includes("ipad")) {
-				alert("iOS에서는 앱 설치가 지원되지 않습니다");
-				return;
-			}
-
-			alert("이미 앱이 설치되어 있거나 앱을 설치할 수 없는 환경입니다");
-			return;
-		}
-
-		deferredPrompt.prompt();
-	};
-
 	return (
 		<div className=" fixed w-full h-full overflow-hidden">
-			<div className=" installApp w-full h-full flex items-center justify-center">
-				<div
-					onClick={installApp}
-					className="border border-black px-10 py-5 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer"
-				>
-					앱 설치하기
-				</div>
-			</div>
+			<InstallPage />
 			<Routes>
 				<Route path="/" Component={Home} />
 				<Route path="/qr" Component={QRView} />
