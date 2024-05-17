cf5c8be62cb9d6165a21e44804068f5d43bcea51
suk-6 <me@suk.kr>
2024-05-17 11:38:22 +0900

    feat: Add skip button to InstallPage component

--- src/pages/install.tsx
+++ src/pages/install.tsx
@@ -13,6 +13,7 @@ interface BeforeInstallPromptEvent extends Event {
 export const InstallPage = () => {
 	const [deferredPrompt, setDeferredPrompt] =
 		useState<BeforeInstallPromptEvent>();
+	const [skiped, setSkiped] = useState<boolean>(false);
 	const userAgent = navigator.userAgent.toLowerCase();
 
 	useEffect(() => {
@@ -20,6 +21,8 @@ export const InstallPage = () => {
 			event.preventDefault();
 			setDeferredPrompt(event as BeforeInstallPromptEvent);
 		});
+
+		if (localStorage.getItem("skip")) setSkiped(true);
 
 		if (userAgent.includes("kakaotalk")) {
 			location.href =
@@ -31,21 +34,32 @@ export const InstallPage = () => {
 	const installApp = () => {
 		if (!deferredPrompt) {
 			if (userAgent.includes("iphone") || userAgent.includes("ipad")) {
-				alert("iOS에서는 앱 설치가 지원되지 않습니다");
+				alert("iOS에서는 아래 지침을 따라 설치해주세요.");
 				return;
 			}
 
-			alert("이미 앱이 설치되어 있거나 앱을 설치할 수 없는 환경입니다");
+			alert("이미 앱이 설치되어 있거나 앱을 설치할 수 없는 환경입니다.");
 			return;
 		}
 
 		deferredPrompt.prompt();
 	};
 
+	const skip = () => {
+		setSkiped(true);
+		localStorage.setItem("skip", "true");
+	};
+
 	return (
-		<div className=" installApp w-full h-full bg-white">
+		<div
+			className={
+				skiped === true
+					? "hidden"
+					: " installApp w-full h-full bg-white"
+			}
+		>
 			<TitleBar title="앱 설치 안내" />
-			<div className="mb-10">
+			<div>
 				<span className="flex items-center justify-center mb-3">
 					안드로이드는 아래 버튼을 터치하여 앱을 설치해주세요.
 				</span>
@@ -58,10 +72,30 @@ export const InstallPage = () => {
 					</div>
 				</div>
 			</div>
+			<div className=" w-full flex justify-center items-center py-5">
+				<div className="w-[95%] border-[1px] border-black" />
+			</div>
 			<div>
-				<span className="flex items-center justify-center">
+				<span className="flex items-center justify-center mb-3">
 					아이폰 사용자는 아래 지시를 따라주세요.
 				</span>
+				<img src="ios.png" alt="" />
+			</div>
+			<div className=" w-full flex justify-center items-center py-5">
+				<div className="w-[95%] border-[1px] border-black" />
+			</div>
+			<div>
+				<span className="flex items-center justify-center mb-3">
+					웹 버전을 이용하시려면 아래 버튼을 터치해주세요.
+				</span>
+				<div className="flex items-center justify-center">
+					<div
+						onClick={skip}
+						className="border-[0.05rem] border-black px-14 py-4 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer"
+					>
+						스킵하기
+					</div>
+				</div>
 			</div>
 		</div>
 	);
