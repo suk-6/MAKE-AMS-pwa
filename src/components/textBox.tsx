73694fe7eb195ed60c86b770d8bb536af0e47746
suk-6 <me@suk.kr>
2024-05-18 17:55:11 +0900

    feat: Add placeholder prop to Textbox component

--- src/components/textBox.tsx
+++ src/components/textBox.tsx
@@ -3,6 +3,7 @@ interface TextboxProps {
 	inputBoxHint: string;
 	value: string;
 	autoComplete?: string;
+	placeholder?: string;
 	changeValue: (value: string) => void;
 }
 
@@ -11,17 +12,19 @@ export const Textbox = ({
 	inputBoxHint,
 	value,
 	autoComplete,
+	placeholder,
 	changeValue,
 }: TextboxProps) => (
 	<div className="flex justify-center items-center w-full h-fit">
-		<div className=" flex flex-col items-start w-auto h-min">
-			<span className="h-min flex justify-start items-center text-black text-base font-normal font-['Pretendard']">
+		<div className=" flex flex-col items-start w-[70%] h-min">
+			<span className="w-max h-min flex justify-start items-center text-black text-base font-normal font-['Pretendard']">
 				{inputBoxHint}
 			</span>
-			<div className="h-10 w-72 flex flex-col">
+			<div className="h-10 w-full flex flex-col">
 				<input
 					type={inputBoxType}
 					value={value}
+					placeholder={placeholder}
 					autoComplete={autoComplete}
 					onChange={(e) => changeValue(e.target.value)}
 					className=" border-0 border-b-2 w-full h-full text-black text-base font-normal font-['Pretendard'] focus:outline-none"
