802c74adaf130697a9e92f4d38ea99b2e3022a98
suk-6 <me@suk.kr>
2024-05-18 17:46:29 +0900

    refactor: Update Textbox component styles and structure

--- src/components/textBox.tsx
+++ src/components/textBox.tsx
@@ -14,17 +14,19 @@ export const Textbox = ({
 	changeValue,
 }: TextboxProps) => (
 	<div className="flex justify-center items-center w-full h-fit">
-		<div className=" grid grid-rows-2 w-auto h-min">
+		<div className=" flex flex-col items-start w-auto h-min">
 			<span className="h-min flex justify-start items-center text-black text-base font-normal font-['Pretendard']">
 				{inputBoxHint}
 			</span>
-			<input
-				type={inputBoxType}
-				value={value}
-				autoComplete={autoComplete}
-				onChange={(e) => changeValue(e.target.value)}
-				className="border border-black h-8"
-			/>
+			<div className="h-10 w-72 flex flex-col">
+				<input
+					type={inputBoxType}
+					value={value}
+					autoComplete={autoComplete}
+					onChange={(e) => changeValue(e.target.value)}
+					className=" border-0 border-b-2 w-full h-full text-black text-base font-normal font-['Pretendard'] focus:outline-none"
+				/>
+			</div>
 		</div>
 	</div>
 );
