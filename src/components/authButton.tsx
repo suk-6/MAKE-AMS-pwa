bf0f9944da1b14d56300a3bf800c6a3ebe2d2a10
suk-6 <me@suk.kr>
2024-05-18 17:46:26 +0900

    refactor: Update AuthButton component styles

--- src/components/authButton.tsx
+++ src/components/authButton.tsx
@@ -4,7 +4,7 @@ interface AuthButtonProps {
 
 export const AuthButton = ({ text }: AuthButtonProps) => {
 	return (
-		<div className=" hover:bg-gray-300 w-4/6 h-full bg-gray-100 rounded-[20px] border-[0.05rem] border-black text-black text-xl font-semibold font-['Pretendard'] flex justify-center items-center">
+		<div className=" hover:bg-gray-100 w-4/6 h-full bg-white border border-black text-black text-xl font-semibold font-['Pretendard'] flex justify-center items-center">
 			{text}
 		</div>
 	);
