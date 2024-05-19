de93d67afb381ebd6538afde83a1f2271fb0bbd4
suk-6 <me@suk.kr>
2024-05-19 23:42:45 +0900

    feat: Add new colors and background gradient to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       current: 'currentColor',
       white: '#FFFFFF',
       black: '#000000',
+      black1: '#191919',
       gray: colors.gray,
       red1: '#E65C5C',
       blue1: '#5C93E6',
@@ -19,8 +20,14 @@ export default {
       green1: '#5CE66A',
       cyan1: '#81EEDB',
       yellow1: '#EBF000',
+      gr1: '#0d4af3',
+      gr2: '#199af6',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-24': 'linear-gradient(24deg, var(--tw-gradient-stops))'
+      },
+    },
   },
   plugins: [],
 }
