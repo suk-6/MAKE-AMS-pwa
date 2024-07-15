ff3b4633a0ce8f6de7efdaec186bb851265040cc
suk-6 <me@suk.kr>
2024-07-15 23:06:20 +0900

    refactor: Improve error handling in admin service functions

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -4,19 +4,27 @@ import { API_BASE_URL } from "../config";
 export const getUsersPendingSignup = async () => {
 	return await fetch(
 		`${API_BASE_URL}/admin/pendings?code=${localStorage.getItem("code")}`
-	).then((res) => {
-		if (res.status !== 200) return [];
-		return res.json();
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return [];
+			return res.json();
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const getAccessLogs = async (): Promise<AccessViewProps[]> => {
 	return await fetch(
 		`${API_BASE_URL}/admin/access-logs?code=${localStorage.getItem("code")}`
-	).then((res) => {
-		if (res.status !== 200) return [];
-		return res.json();
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return [];
+			return res.json();
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const approveAdmin = async (id: string) => {
@@ -24,10 +32,14 @@ export const approveAdmin = async (id: string) => {
 		`${API_BASE_URL}/auth/setadmin?code=${localStorage.getItem(
 			"code"
 		)}&id=${id}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const approveUser = async (id: string) => {
@@ -35,10 +47,14 @@ export const approveUser = async (id: string) => {
 		`${API_BASE_URL}/admin/approve?code=${localStorage.getItem(
 			"code"
 		)}&id=${id}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const rejectUser = async (id: string) => {
@@ -46,42 +62,62 @@ export const rejectUser = async (id: string) => {
 		`${API_BASE_URL}/admin/reject?code=${localStorage.getItem(
 			"code"
 		)}&id=${id}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const getLockStatus = async () => {
-	return await fetch(`${API_BASE_URL}/admin/door-status`).then((res) => {
-		if (res.status !== 200) return "error";
-		return res.json();
-	});
+	return await fetch(`${API_BASE_URL}/admin/door-status`)
+		.then((res) => {
+			if (res.status !== 200) return "error";
+			return res.json();
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const lockDoor = async () => {
 	return await fetch(
 		`${API_BASE_URL}/admin/lock?code=${localStorage.getItem("code")}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const unlockDoor = async () => {
 	return await fetch(
 		`${API_BASE_URL}/admin/unlock?code=${localStorage.getItem("code")}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
 
 export const restrictDoor = async () => {
 	return await fetch(
 		`${API_BASE_URL}/admin/restrict?code=${localStorage.getItem("code")}`
-	).then((res) => {
-		if (res.status !== 200) return false;
-		return true;
-	});
+	)
+		.then((res) => {
+			if (res.status !== 200) return false;
+			return true;
+		})
+		.catch((e) => {
+			alert(e);
+		});
 };
